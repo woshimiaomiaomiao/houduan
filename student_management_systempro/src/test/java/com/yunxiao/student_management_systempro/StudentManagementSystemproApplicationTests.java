28bd0a1f206a762c879b418d3889ed2481caf999
woshimiaomiaomiao <160444080+woshimiaomiaomiao@users.noreply.github.com>
2024-02-22 12:32:51 +0800

    Add files via upload

---
+++ student_management_systempro/src/test/java/com/yunxiao/student_management_systempro/StudentManagementSystemproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunxiao.student_management_systempro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
