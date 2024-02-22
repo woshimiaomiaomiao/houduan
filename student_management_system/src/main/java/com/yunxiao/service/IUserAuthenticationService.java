79aafb2c0a9f5825424a7d30c1ba1eae903ef558
woshimiaomiaomiao <160444080+woshimiaomiaomiao@users.noreply.github.com>
2024-02-22 09:06:47 +0800

    Delete student_management_system directory

--- student_management_system/src/main/java/com/yunxiao/service/IUserAuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.yunxiao.service;
-//注册与登录接口
-public interface IUserAuthenticationService {
-    // 注册账号
-    void register(String username, String password);
-
-    // 登录账号
-    boolean login(String username, String password);
-}
